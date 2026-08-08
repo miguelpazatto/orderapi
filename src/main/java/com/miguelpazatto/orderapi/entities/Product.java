82ab13155e1d657e55ab7df3f56856dee8647454
miguelpazatto <miguelpazatto60@gmail.com>
2026-08-08 13:28:48 -0300

    feat: configura infraestrutura docker e modela entidade Produto

---
+++ src/main/java/com/miguelpazatto/orderapi/entities/Product.java
@@ -0,0 +1,43 @@
+package com.miguelpazatto.orderapi.entities;
+
+import com.miguelpazatto.orderapi.entities.enums.ProductStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer availableStock;
+
+    @Column(unique = true, nullable = false)
+    private String sku;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProductStatus productStatus;
+
+}
---
+++ src/main/java/com/miguelpazatto/orderapi/entities/enums/ProductStatus.java
@@ -0,0 +1,9 @@
+package com.miguelpazatto.orderapi.entities.enums;
+
+public enum ProductStatus {
+
+    ACTIVE,
+    INACTIVE,
+    OUT_OF_STOCK
+
+}
