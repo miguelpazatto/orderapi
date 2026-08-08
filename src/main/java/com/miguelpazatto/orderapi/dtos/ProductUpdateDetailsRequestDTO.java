356966bd48524b7c2a574c7b43065ec244c03588
miguelpazatto <miguelpazatto60@gmail.com>
2026-08-08 19:10:18 -0300

    feat: cria e configura controller da entidade produto

---
+++ src/main/java/com/miguelpazatto/orderapi/dtos/ProductUpdateDetailsRequestDTO.java
@@ -0,0 +1,8 @@
+package com.miguelpazatto.orderapi.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ProductUpdateDetailsRequestDTO(
+        @NotBlank(message = "O nome do produto não pode estar vazio") String name,
+        @NotBlank(message = "A descrição do produto não pode estar vazia") String description) {
+}
