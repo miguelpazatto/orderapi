9cbee64c8dbe16c6321b78472593b5dd898d7ac2
miguelpazatto <miguelpazatto60@gmail.com>
2026-08-17 16:23:32 -0300

    chore: adiciona dependencia e habilita spring cloud com open feign

--- src/main/java/com/miguelpazatto/orderapi/delivery/services/DeliveryService.java
+++ src/main/java/com/miguelpazatto/orderapi/delivery/services/DeliveryService.java
@@ -1,7 +1,30 @@
 package com.miguelpazatto.orderapi.delivery.services;
 
+import com.miguelpazatto.orderapi.core.exceptions.ResourceNotFoundException;
+import com.miguelpazatto.orderapi.delivery.entities.Delivery;
+import com.miguelpazatto.orderapi.delivery.repositories.DeliveryRepository;
+import com.miguelpazatto.orderapi.orders.entities.Order;
+import com.miguelpazatto.orderapi.orders.repositories.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+import java.util.UUID;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class DeliveryService {
+
+    private final DeliveryRepository deliveryRepository;
+
+    @Transactional
+    public void criarEntregaParaPedido(UUID orderId) {
+        Delivery delivery = new Delivery(orderId);
+        deliveryRepository.save(delivery);
+
+        log.info("Registro de entrega criado com sucesso para o pedido {}.", orderId);
+    }
 }
